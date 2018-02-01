b2ff9fb829de6c2c0b1d31085342f91f5785d9f1
Gabriel Bergerongit config --global user.email gabriel.bergeron3@usherbrooke.ca <gabriel.bouleau@gmail.com>
2018-02-01 09:28:12 -0500

    test

---
+++ R/mangal.R
@@ -0,0 +1,10 @@
+#' mangal: A package to ....
+#'
+#' The description of the package
+#'
+#' @section mangal functions:
+#'   Describe mangal function here
+#'
+#' @docType package
+#' @name mangal
+NULL
