2efa1477a7bad9f46336728adadbb75762716a21
Gabriel Bergerongit config --global user.email gabriel.bergeron3@usherbrooke.ca <gabriel.bouleau@gmail.com>
2018-02-14 15:05:14 -0500

    Minor bug fix

--- R/POST/POST_tables/POST_interactions.R
+++ R/POST/POST_tables/POST_interactions.R
@@ -31,7 +31,7 @@ POST_interactions <- function(inter_df){
     try(inter_df[i, "taxon_2"] <- GET_fkey("taxons", "name", inter_df[i,2]))
   }
 
-  inter_df[, "attr_id"]        <- GET_fkey("traits", "name", attr[[1]])
+  inter_df[, "attr_id"]        <- GET_fkey("attributes", "name", attr[[1]])
   inter_df[, "environment_id"] <- GET_fkey("environments", "name", enviro[[1]])
   inter_df[, "network_id"]     <- GET_fkey("networks", "name", networks[[1]])
   inter_df[, "ref_id"]         <- GET_fkey("refs", "doi", refs[[1]])
@@ -42,10 +42,10 @@ POST_interactions <- function(inter_df){
 
   # Add metadata
   inter_df <- cbind(data.table::setDT(inter_df),
-                    data.table::setDT(as.data.frame(inter)), fill = T)
+                    data.table::setDT(as.data.frame(inter)))
 
   # inter_df as a json list
-  inter_lst <- json_list(inter_df)
+  inter_lst <- json_list(as.data.frame(inter_df))
 
   # Inject to interactions table
   POST_table(inter_lst, "interactions")
