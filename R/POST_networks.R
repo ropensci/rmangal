b9e588c0ac3ba37f1289cbca408faf9a9a586f2a
Gabriel Bergerongit config --global user.email gabriel.bergeron3@usherbrooke.ca <gabriel.bouleau@gmail.com>
2018-03-05 11:53:39 -0500

    Change in R folder structure

--- R/POST_networks.R
+++ R/POST_networks.R
@@ -36,7 +36,7 @@ POST_networks <- function(){
   config <- httr::add_headers("Content-type" = "application/json")
 
   path <- httr::modify_url(server, path = paste0("/api/v0/","networks/?name=",
-                                          networks[[1]]))
+                                          networks_lst[[1]]))
   # Change space in url by "_"
   path <- gsub(" ", "%20", path)
 
@@ -45,28 +45,28 @@ POST_networks <- function(){
 
     # Retrive foreign key
     if (length(content(httr::GET(url = gsub(" ", "%20", paste0(server, "/api/v0/datasets/?name=", datasets[[1]])), config = config))) != 0){
-      networks <- c(networks, dataset_id = GET_fkey("datasets", "name", datasets[[1]]))
+      networks_lst <- c(networks_lst, dataset_id = GET_fkey("datasets", "name", datasets[[1]]))
     }
 
     if (length(content(httr::GET(url = gsub(" ", "%20", paste0(server, "/api/v0/refs/?doi=", refs[[1]])), config = config))) != 0){
-      networks <- c(networks, ref_id = GET_fkey("refs", "doi", refs[[1]]))
+      networks_lst <- c(networks_lst, ref_id = GET_fkey("refs", "doi", refs[[1]]))
     }
 
     if (length(content(httr::GET(url = gsub(" ", "%20", paste0(server, "/api/v0/environments/?name=", enviro[[1]])), config = config))) != 0){
-      networks <- c(networks, environment_id = GET_fkey("environments", "name", enviro[[1]]))
+      networks_lst <- c(networks_lst, environment_id = GET_fkey("environments", "name", enviro[[1]]))
     }
 
     if (length(content(httr::GET(url = gsub(" ", "%20", paste0(server, "/api/v0/users/?name=", users[[1]])), config = config))) != 0){
-      networks <- c(networks, user_id = GET_fkey("users", "name", users[[1]]))
+      networks_lst <- c(networks_lst, user_id = GET_fkey("users", "name", users[[1]]))
     }
 
     # attach location to the network
-    geoloc <- geojsonio::geojson_list(c(networks$lat,networks$lon))$features[[1]]$geometry
-    geoloc$crs <- list(type="name",properties=list(name=paste0("EPSG:",networks$srid)))
-    networks$localisation <- geoloc
+    geoloc <- geojsonio::geojson_list(c(networks_lst$lat,networks_lst$lon))$features[[1]]$geometry
+    geoloc$crs <- list(type="name",properties=list(name=paste0("EPSG:",networks_lst$srid)))
+    networks_lst$localisation <- geoloc
 
     # networks_df as a json list
-    networks[c("lat","lon","srid")] <- NULL
+    networks_lst[c("lat","lon","srid")] <- NULL
     networks_lst <- json_list(networks)
 
     # Inject to networks table
