d47a01c65bfdbc291302f6f2c34be5c760826b16
Gabriel <gabriel.bergeron3@usherbrooke.ca>
2018-04-17 16:17:28 -0400

    delete function removed

--- R/DELETE_loop.R
+++
@@ -1,8 +0,0 @@
-#for (i in 1:1000) {
-#  DELETE( url = paste0("http://localhost:3000/api/v0/taxon/", i))
-#}
-
-#for (i in 1:1000) {
-#  DELETE(url = paste0("http://localhost:3000/api/v0/taxo_back/", i))
-#}
-
