33f022c5e185d94be8aac93963b60b46a3c00658
Steve Vissault <s.vissault@yahoo.fr>
2019-03-11 13:20:04 -0400

    init zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,21 @@
-#.onAttach <- function(libname, "mangal package name"){
-#    packageStartupMessage("Message")
+# Config de base
+server <- function() "http://poisotlab.biol.umontreal.ca"
+#server <- function() "http://localhost:8080" # dev purpose
+base <- function() "/api/v2"
+bearer <- function() ifelse(file.exists(".httr-oauth"), as.character(readRDS(".httr-oauth")), NA)
+ua <- httr::user_agent("rmangal")
+
+# Point d'entrées de l'API
+endpoints <- function(){
+  list(
+    dataset = "/dataset",
+    environment = "/environment",
+    interaction = "/interaction",
+    network = "/network",
+    node = "/node",
+    reference = "/reference",
+    attribute = "/attribute",
+    taxonomy = "/taxonomy",
+    trait = "/trait"
+  )
+}
