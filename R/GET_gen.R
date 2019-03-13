2bf238b8079cc8047884c7b88eb1e08fd88f42a0
Steve Vissault <s.vissault@yahoo.fr>
2019-03-13 14:12:44 -0400

    Cleaning generic get

--- R/GET_gen.R
+++ R/GET_gen.R
@@ -5,7 +5,6 @@
 #' @param limit `integer` number of entries return by the API (max: 1000)
 #' @param flatten `logical` flatten nested data.frame, see [jsonlite::flatten()]
 #' @param output `character` output type: `data.frame`, `list`, `json`
-#' @param token  `character` Access bearer token (mandatory)
 #' @param ... httr options, see [httr::GET()]
 #' @return
 #' Object of class `getSuccess` or `getError`.
@@ -16,22 +15,18 @@
 #' @examples
 #' @export
 
-get_gen <- function(endpoint = NULL, query = NULL, limit =100, flatten = TRUE, output = 'data.frame', token = bearer(), ...) {
+get_gen <- function(endpoint = NULL, query = NULL, limit =100, flatten = TRUE, output = 'data.frame', ...) {
 
   stopifnot(exists("endpoint") | is.null(endpoint))
 
   url <- httr::modify_url(server(), path = paste0(base(), endpoint))
 
-  # Remove NA for querying
-  if(!is.null(query)) query[which(is.na(query) | query == "NA")] <- NULL
-
-  # Get number of pages
-  resp <- httr::GET(url, config = httr::add_headers(`Content-type` = "application/json",
-    Authorization = paste("Bearer", token)),ua, query = query, ...)
+  # First call used to set pages
+  resp <- httr::GET(url, config = httr::add_headers(`Content-type` = "application/json"),ua, query = query, ...)
   
   # Prep output object
   responses <- list()
-  class(responses) <- "mangalGetResp"
+  class(responses) <- "mangalGet"
 
   # Cover status is 401
   if(httr::status_code(resp) == 401){
@@ -44,18 +39,13 @@ get_gen <- function(endpoint = NULL, query = NULL, limit =100, flatten = TRUE, o
     "\\(?[0-9,.]+\\)?"))
 
   # Prep iterator over pages
-  if (rg[3] < limit) {
-    pages <- 0
-  } else {
-    pages <- ceiling(rg[3] / limit) - 1
-  }
+  pages <- ifelse(rg[3] < limit, 0, floor(rg[3] / limit))
 
   # Loop over pages
   for (page in 0:pages) {
     query$page <- page
 
-    resp <- httr::GET(url, config = httr::add_headers(`Content-type` = "application/json",
-      Authorization = paste("Bearer", token)), ua, query = query, ...)
+    resp <- httr::GET(url, config = httr::add_headers(`Content-type` = "application/json"), ua, query = query, ...)
 
     # prep output
     if(output == 'json'){
@@ -63,27 +53,7 @@ get_gen <- function(endpoint = NULL, query = NULL, limit =100, flatten = TRUE, o
     } else if(output == 'list') {
       body <- httr::content(resp)
     } else if(output == 'data.frame') {
-      body <- tibble::as_tibble(jsonlite::fromJSON(httr::content(resp, type = "text", encoding = "UTF-8"), flatten = TRUE, simplifyDataFrame = TRUE))
-    } else if(output == 'sf') { 
-        body <- tibble::as_tibble(jsonlite::fromJSON(httr::content(resp, type = "text", encoding = "UTF-8"), flatten = TRUE, simplifyDataFrame = TRUE))
-        stopifnot(all(c("localisation.type", "localisation.coordinates") %in% names(body)))
-        features <- apply(body, 1, function(feature){
-              return(list(type="Feature", geometry=list(type=feature$localisation.type,coordinates=feature$localisation.coordinates)))
-            })
-        
-        body <- dplyr::select(body, -c(localisation.type, localisation.coordinates))
-
-        # get sf geom
-        geom_s <- sf::read_sf(
-          jsonlite::toJSON(
-            list(
-              type="FeatureCollection",
-              features=features),
-              auto_unbox=TRUE
-          )
-        )
-
-        body <- sf::st_sf(dplyr::bind_cols(body,geom_s), sf_column_name = "geometry")
+      body <- tibble::as_tibble(jsonlite::fromJSON(httr::content(resp, type = "text", encoding = "UTF-8"), flatten = flatten))
     }
 
     if (httr::http_error(resp)) {
