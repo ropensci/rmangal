4b13e9aee8507d6669859e34fb232e6a43ac5a96
Kevin Cazelles <kcazelle@uoguelph.ca>
2019-03-15 13:22:19 -0400

    remove unnecessary calls to return()

--- R/list_datasets.R
+++ R/list_datasets.R
@@ -1,6 +1,6 @@
 #' List mangal datasets with networks and reference attached
 #'
-#' @param search `character` keyword to search (case sensitive). 
+#' @param search `character` keyword to search (case sensitive).
 #' @param ... arguments from [rmangal::get_gen()]
 #' @return
 #' object `data.frame`: Datasets with all networks and the original reference attached.
@@ -15,14 +15,14 @@
 list_datasets <- function( search = NULL, ... ) {
 
     datasets <- as.data.frame(get_gen(endpoints()$dataset, query = list( q = search), ...))
-    
+
     if(!is.null(search)) message(sprintf("Found %s dataset(s) for keywork: %s", nrow(datasets), search))
 
     # Attached reference and network
     networks <- list()
     references <- list()
 
-    for(i in 1:nrow(datasets)){ 
+    for(i in 1:nrow(datasets)){
         networks[[i]] <- purrr::map_df(get_fkey(endpoints()$network, column = "dataset_id", id = datasets[i,"id"]), "body")
         references[[i]] <- purrr::map_df(get_singletons(endpoints()$reference, ids =  datasets[i,"ref_id"], output = "data.frame"), "body")
     }
@@ -30,7 +30,6 @@ list_datasets <- function( search = NULL, ... ) {
     datasets$networks <- networks
     datasets$references <- references
 
-    return(datasets)
+    datasets
 
 }
-
