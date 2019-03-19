26bf5b8d87e670540082b9c31808091a5d801525
Steve Vissault <s.vissault@yahoo.fr>
2019-03-19 16:44:36 -0400

    New S3 methods - get_collection

--- R/get_collection.R
+++ R/get_collection.R
@@ -1,4 +1,4 @@
-#' Retrieve mangal networks collection
+#' Get collection of networks 
 #'
 #' @param x `numeric` vector of mangal network IDs or object return by functions `search_*`
 #' @param ... arguments from [rmangal::get_network_by_id()]
@@ -18,48 +18,52 @@ get_collection <- function (x, ...) {
 #' @export
 get_collection.default <- function(x, ... ) {
   
-  mg_networks_collection <- structure(
+  mg_net_collection <- structure(
     list(networks = list()), 
   class = "mgNetworksCollection")
 
   for(i in seq_len(length(x))){
-    mg_networks_collection$networks[[i]] <- get_network_by_id(x[i], ...)
+    mg_net_collection$networks[[i]] <- get_network_by_id(x[i], ...)
   }
 
-  mg_networks_collection
+  mg_net_collection
 
 }
 
+#' Get collection of networks from mgSearchDatasets
+#' @describeIn get_collection
 #' @export
 get_collection.mgSearchDatasets <- function(x, ... ) {
   
   # Object S3 declaration
-  mg_networks_collection <- structure(
+  mg_net_collection <- structure(
     list(networks = list()), 
   class = "mgNetworksCollection")
 
   ids <- purrr::flatten_int(purrr::map(x$networks,"id"))
 
   for(i in seq_len(nrow(x))){
-    mg_networks_collection$networks[[i]] <- get_network_by_id(ids[i], ...)
+    mg_net_collection$networks[[i]] <- get_network_by_id(ids[i], ...)
   }
 
-  mg_networks_collection
+  mg_net_collection
 
 }
 
+#' Get collection of networks from mgSearchNetworks
+#' @describeIn get_collection
 #' @export
 get_collection.mgSearchNetworks <- function(x, ... ) {
   
   # Object S3 declaration
-  mg_networks_collection <- structure(
+  mg_net_collection <- structure(
     list(networks = list()), 
   class = "mgNetworksCollection")
 
   for(i in seq_len(length(x$id))){
-    mg_networks_collection$networks[[i]] <- get_network_by_id(x$id[i], ...)
+    mg_net_collection$networks[[i]] <- get_network_by_id(x$id[i], ...)
   }
 
-  mg_networks_collection
+  mg_net_collection
 
 }
