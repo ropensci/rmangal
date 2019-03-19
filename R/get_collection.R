09b78f069f0b0708dd1df57ea54a13fe344fbc67
Steve Vissault <s.vissault@yahoo.fr>
2019-03-19 15:44:48 -0400

    Finished class mgNetworkCollection - issue #12

--- R/get_collection.R
+++ R/get_collection.R
@@ -1,26 +1,60 @@
 #' Retrieve mangal networks collection
 #'
-#' @param ids `numeric` vector of mangal network IDs or object `mangalSearch` returned by functions `search_*`
+#' @param ids `numeric` vector of mangal network IDs or object returned by functions `search_*`
 #' @param ... arguments from [rmangal::get_network_by_id()]
 #' @return
 # object `mgNetworksCollection`: 
 #' - networks: `list` of object `mgNetwork` (see [rmangal::get_network_by_id()])
 #' @examples
 #' get_collection(ids = c(1035:1037))
+#' get_collection(search_networks(query="insect%"))
+#' get_collection(search_datasets(query="lagoon%"))
 #' @export
 
-get_collection <- function(ids , ... ) {
+ get_collection <- function (x, ...) {
+  UseMethod("get_collection", x)
+ }
 
+get_collection.numeric <- function(x, ... ) {
+  
+  mg_networks_collection <- structure(
+    list(networks = list()), 
+  class = "mgNetworksCollection")
+
+  for(i in seq_len(length(x))){
+    mg_networks_collection$networks[[i]] <- get_network_by_id(ids[i], ...)
+  }
+
+  mg_networks_collection
+
+}
+
+get_collection.mgSearchDatasets <- function(x, ... ) {
+  
   # Object S3 declaration
   mg_networks_collection <- structure(
-    list(
-      networks = list() # list of mgNetwork class
-    ),
-    class = "mgNetworksCollection"
-  )
-  
-  for(i in seq_len(length(ids))){
+    list(networks = list()), 
+  class = "mgNetworksCollection")
+
+  ids <- purrr::flatten_int(purrr::map(x$networks,"id"))
+
+  for(i in seq_len(nrow(x))){
     mg_networks_collection$networks[[i]] <- get_network_by_id(ids[i], ...)
+  }
+
+  mg_networks_collection
+
+}
+
+get_collection.mgSearchNetworks <- function(x, ... ) {
+  
+  # Object S3 declaration
+  mg_networks_collection <- structure(
+    list(networks = list()), 
+  class = "mgNetworksCollection")
+
+  for(i in seq_len(length(x$id))){
+    mg_networks_collection$networks[[i]] <- get_network_by_id(x$id[i], ...)
   }
 
   mg_networks_collection
