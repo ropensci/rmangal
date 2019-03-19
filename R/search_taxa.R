e97bef6c051f62f9b0391e3ecb532690865ac7d8
Steve Vissault <s.vissault@yahoo.fr>
2019-03-18 22:04:46 -0400

    Init search_taxa()

---
+++ R/search_taxa.R
@@ -0,0 +1,14 @@
+#' List mangal datasets
+#'
+#' @param query `character` keyword used to search (case sensitive), or `list` of custom query.
+#' @param ... arguments from [rmangal::get_gen()]
+#' @return
+#' object `data.frame`: Taxa information with all interactions in which the taxa occurs.
+#' @examples
+#' @export
+
+search_taxa <- function( query = NULL, ... ) {
+
+
+
+}
