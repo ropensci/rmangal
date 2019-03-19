6a6dd9293b45e97ccdedb5630ec0c2ca57dc4b83
Steve Vissault <s.vissault@yahoo.fr>
2019-03-18 22:02:38 -0400

    Renamed to get_network_by_id(); add ref and nets to mgNetwork

---
+++ R/get_network_by_id.R
@@ -0,0 +1,46 @@
+#' Retrieve mangal network including the nodes and the edges
+#'
+#' @param id `numeric` mangal ID network
+#' @param ... arguments from [rmangal::get_singletons()]
+#' @return
+#' object `mgNetwork`: 
+#' - network: `list` of all generic informations on the network
+#' - nodes: `data.frame` of all nodes with taxonomic informations
+#' - edges: `data.frame` of all edges (ecological interactions), with the attribute used to describe the interaction
+#' - dataset: `list` of all informations on the dataset associated to the network
+#' - reference: `list` of all informations on the original publication
+#' @examples
+#' get_network_by_id(id = 18)
+#' @export
+
+
+get_network_by_id <- function(id, ... ) {
+
+    # Object S3 declaration
+    mg_network <- structure(
+      list(
+        network = get_singletons(endpoints()$network, ids = id)[[1L]]$body
+      ),
+      class = "mgNetwork"
+    )
+
+    # nodes and edges associated with the network
+    mg_network$nodes <- as.data.frame(get_gen(endpoints()$node,
+      query = list( network_id = mg_network$network$id )))
+    mg_network$edges <- as.data.frame(get_gen(endpoints()$interaction,
+      query = list( network_id = mg_network$network$id )))
+
+    # retrieve DATASET informations
+    dataset <- get_singletons(endpoints()$dataset, ids = unique(mg_network$network$dataset_id))
+
+    # Assign dataset informations to mg_network
+    mg_network$dataset <- unlist(purrr::map(dataset,"body"), recursive = FALSE)
+
+    # retrieve REFERENCE 
+    reference <- get_singletons(endpoints()$reference, ids = mg_network$dataset$ref_id)
+
+    # Assign dataset informations to mg_network
+    mg_network$reference <- unlist(purrr::map(reference,"body"), recursive = FALSE)
+
+    mg_network
+}
