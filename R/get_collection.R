6546cd643167267d37725be48b596a302fecc1af
Steve Vissault <s.vissault@yahoo.fr>
2019-03-18 22:01:50 -0400

    Init fun get_collections()

---
+++ R/get_collection.R
@@ -0,0 +1,28 @@
+#' Retrieve mangal networks collection
+#'
+#' @param ids `numeric` vector of mangal network IDs or object `mangalSearch` returned by functions `search_*`
+#' @param ... arguments from [rmangal::get_network_by_id()]
+#' @return
+# object `mgNetworksCollection`: 
+#' - networks: `list` of object `mgNetwork` (see [rmangal::get_network_by_id()])
+#' @examples
+#' get_collection(ids = c(1035:1037))
+#' @export
+
+get_collection <- function(ids , ... ) {
+
+    # Create empty structure of mgNetworksCollections
+    mg_networks_collection <- structure(
+      list(
+        networks = list() # list of mgNetwork class
+      ),
+      class = "mgNetworksCollection"
+    )
+  
+  for(i in seq_len(length(ids))){
+    mg_networks_collection$networks[[i]] <- get_network_by_id(ids[i], ...)
+  }
+
+  mg_networks_collection
+
+}
