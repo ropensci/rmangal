a1b90b0db3d2c4767e936a644df1fadd6b4e7a30
Steve Vissault <s.vissault@yahoo.fr>
2019-03-31 12:11:46 -0400

    Init search_taxonomy

---
+++ R/search_taxonomy.R
@@ -0,0 +1,22 @@
+#' Search network by taxa
+#'
+#' @param query `character` specific taxon / keyword / or tsn, eol, bold & ncbi IDs.
+#' @param original a `logical`. Should query taxa from the original publication? (see details section) 
+#' @param verbose a `logical`. Should extra information be reported on progress?
+#' @details
+#' By default, you are querying taxa based on a homogenized taxonomy across all datasets using
+#' TNRS (http://tnrs.iplantcollaborative.org/) and/or GNR (https://resolver.globalnames.org/). 
+#' Be aware that these taxa might not be the taxa from the original publication. 
+#' To query original documented taxa, please set the argument `original` as TRUE.
+#' @return
+#' An object of class `mgSearchTaxonomy`, which is a `data.frame` object with all taxa matching the query. 
+#' All networks in which taxa are involved are also attached to the `data.frame`.
+#' @export
+
+search_taxa <- function( query = NULL ) {
+
+    stopifnot(!is.null(query) & is.character(query))
+
+
+
+}
