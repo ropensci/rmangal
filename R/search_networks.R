43e36fd73a386a6a5c82373adb1ef8e24a4e58c8
Steve Vissault <s.vissault@yahoo.fr>
2019-03-31 12:12:39 -0400

    Uniform docs :memo:

--- R/search_networks.R
+++ R/search_networks.R
@@ -1,7 +1,7 @@
 #' Search mangal networks
 #'
 #' @param query `character` keyword used to search (case sensitive) or a `sf` object used to search in a specific geographical area.
-#' @param verbose a logical. Should extra information be reported on progress?
+#' @param verbose a `logical`. Should extra information be reported on progress?
 #' @return
 #' An object of class `mgSearchNetworks`, which is a `data.frame` object with all networks informations
 #' @examples
