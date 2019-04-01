4213af07c9179c7a0156a1828760817b24949033
Steve Vissault <s.vissault@yahoo.fr>
2019-04-01 10:26:23 -0400

    Finished search_taxa - fix #29

--- R/search_taxa.R
+++ R/search_taxa.R
@@ -12,18 +12,34 @@
 #' @return
 #' An object of class `mgSearchTaxonomy`, which is a `data.frame` object with all taxa matching the query. 
 #' All networks in which taxa are involved are also attached to the `data.frame`.
+#' @examples
+#' search_taxa("Acer")
 #' @export
 
 search_taxa <- function( query = NULL, original = FALSE, verbose = TRUE, ... ) {
 
     stopifnot(!is.null(query) & is.character(query))
 
-    taxa <- as.data.frame(get_gen(endpoints()$taxonomy, query = list(q = query)))
-    
-    tmp_nodes <- sapply(taxa$id ,function(x) get_from_fkey(endpoints()$node, taxonomy_id = x ))
-    network_ids <- do.call(rbind,purrr::map(tmp_nodes, "body"))$network_id
+    if ( original == FALSE ) {
 
-    taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
+        taxa <- as.data.frame(get_gen(endpoints()$taxonomy, query = list(q = query)))
+        tmp_nodes <- sapply(taxa$id ,function(x) get_from_fkey(endpoints()$node, taxonomy_id = x ))
+        tmp_nodes <- do.call(rbind,purrr::map(tmp_nodes, "body"))
+
+        # Add original publication name for the taxa
+        taxa$original_name <- tmp_nodes$original_name
+        
+        # Retrieve network in which taxa are involved
+        network_ids <- tmp_nodes$network_id
+        taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
+
+    } else {
+
+        taxa <- as.data.frame(get_gen(endpoints()$node, query = list(q = query)))
+        network_ids <- taxa$network_id
+        taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
+
+    }
 
     if (verbose) message(sprintf("Found %s taxa", nrow(taxa)))
 
