8d79a18783670feb635123f83a5abf9842c1741f
Steve Vissault <s.vissault@yahoo.fr>
2019-04-01 10:14:14 -0400

    set get_taxa() & get_collection method :sparkles:

--- R/search_taxa.R
+++ R/search_taxa.R
@@ -1,12 +1,12 @@
 #' Search network by taxa using keyword, tsn, eol, bold or ncbi IDs.
 #'
 #' @param query `character` specific taxon / keyword / or tsn, eol, bold & ncbi IDs.
-#' @param original a `logical`. Should query taxa from the original publication? (see details section, default: FALSE) 
+#' @param original a `logical`. Should query taxa names from the original publication? (see details section, default: FALSE) 
 #' @param verbose a `logical`. Should extra information be reported on progress?
 #' @param ... further arguments to be passed to [rmangal::get_gen()].
 #' @details
 #' By default, you are querying taxa based on a homogenized taxonomy (corrected names from
-#' TNRS -- http://tnrs.iplantcollaborative.org/ and/or GNR -- https://resolver.globalnames.org/). 
+#' [TNRS](http://tnrs.iplantcollaborative.org/) and/or [GNR](https://resolver.globalnames.org/). 
 #' These taxa might not be the taxa recorded within the original publication. 
 #' To query original documented taxa, please set the argument `original` as TRUE.
 #' @return
@@ -18,6 +18,16 @@ search_taxa <- function( query = NULL, original = FALSE, verbose = TRUE, ... ) {
 
     stopifnot(!is.null(query) & is.character(query))
 
+    taxa <- as.data.frame(get_gen(endpoints()$taxonomy, query = list(q = query)))
+    
+    tmp_nodes <- sapply(taxa$id ,function(x) get_from_fkey(endpoints()$node, taxonomy_id = x ))
+    network_ids <- do.call(rbind,purrr::map(tmp_nodes, "body"))$network_id
 
+    taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
+
+    if (verbose) message(sprintf("Found %s taxa", nrow(taxa)))
+
+    class(taxa) <- append(class(taxa), "mgSearchTaxa")
+    taxa
 
 }
