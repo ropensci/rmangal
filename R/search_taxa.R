a7e5b07c67ffa7d9c3d081f0d2fb648d9ae9bbab
Kevin Cazelles <kcazelle@uoguelph.ca>
2019-04-01 20:48:10 -0400

    test search_taxa() :white_check_mark:

--- R/search_taxa.R
+++ R/search_taxa.R
@@ -1,16 +1,16 @@
 #' Search network by taxa using keyword, tsn, eol, bold or ncbi IDs.
 #'
 #' @param query `character` specific taxon / keyword / or tsn, eol, bold & ncbi IDs.
-#' @param original a `logical`. Should query taxa names from the original publication? (see details section, default: FALSE) 
+#' @param original a `logical`. Should query taxa names from the original publication? (see details section, default is set to `FALSE`)
 #' @param verbose a `logical`. Should extra information be reported on progress?
 #' @param ... further arguments to be passed to [rmangal::get_gen()].
 #' @details
 #' By default, you are querying taxa based on a homogenized taxonomy (corrected names from
-#' [TNRS](http://tnrs.iplantcollaborative.org/) and/or [GNR](https://resolver.globalnames.org/). 
-#' These taxa might not be the taxa recorded within the original publication. 
-#' To query original documented taxa, please set the argument `original` as TRUE.
+#' [TNRS](http://tnrs.iplantcollaborative.org/) and/or [GNR](https://resolver.globalnames.org/).
+#' These taxa might not be the taxon name used in the original publication.
+#' In order to query, the original name, set argument `original` to `TRUE`.
 #' @return
-#' An object of class `mgSearchTaxa`, which is a `data.frame` object with all taxa matching the query. 
+#' An object of class `mgSearchTaxa`, which is a `data.frame` object with all taxa matching the query.
 #' All networks in which taxa are involved are also attached to the `data.frame`.
 #' @examples
 #' search_taxa("Acer")
@@ -23,26 +23,35 @@ search_taxa <- function( query = NULL, original = FALSE, verbose = TRUE, ... ) {
 
     stopifnot(!is.null(query) & is.character(query))
 
-    if ( original == FALSE ) {
+    if (!original) {
 
         taxa <- as.data.frame(get_gen(endpoints()$taxonomy, query = list(q = query)))
-        tmp_nodes <- sapply(taxa$id ,function(x) get_from_fkey(endpoints()$node, taxonomy_id = x ))
-        tmp_nodes <- do.call(rbind,purrr::map(tmp_nodes, "body"))
 
-        # Add original publication name for the taxa
-        taxa$original_name <- tmp_nodes$original_name
-        
-        # Retrieve network in which taxa are involved
-        network_ids <- tmp_nodes$network_id
-        taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
+        if (length(taxa)) {
+          tmp_nodes <- do.call(rbind,
+              purrr::map(sapply(taxa$id,
+                function(x) get_from_fkey(endpoints()$node, taxonomy_id = x )), "body"))
+
+          # Add original publication name for the taxa
+          taxa$original_name <- tmp_nodes$original_name
+          # Retrieve network in which taxa are involved
+          network_ids <- tmp_nodes$network_id
+        } else network_ids <- NULL
 
     } else {
 
-        taxa <- as.data.frame(get_gen(endpoints()$node, query = list(q = query)))
+        taxa <- as.data.frame(get_gen(endpoints()$node,
+          query = list(q = query)))
         network_ids <- taxa$network_id
-        taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
 
     }
+
+    if (length(network_ids)) {
+        taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
+    } else {
+      taxa <- data.frame(NULL)
+    }
+
 
     if (verbose) message(sprintf("Found %s taxa", nrow(taxa)))
 
