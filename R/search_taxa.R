c5df8fda00d082a532a022c464d5598608141a03
Steve Vissault <s.vissault@yahoo.fr>
2019-06-13 11:31:00 -0400

    Query on IDs in search_taxa :sparkles: - #29

--- R/search_taxa.R
+++ R/search_taxa.R
@@ -1,6 +1,6 @@
 #' Search network by taxa using keyword, tsn, eol, bold or ncbi IDs.
 #'
-#' @param query `character` specific taxon / keyword / or tsn, eol (page ID), bold & ncbi IDs (mandatory arg).
+#' @param query `character` query on taxa names
 #' @param original a `logical`. Should query taxa names from the original publication? (see details section, default is set to `FALSE`)
 #' @param verbose a `logical`. Should extra information be reported on progress?
 #' @param ... further arguments to be passed to [rmangal::get_gen()].
@@ -19,41 +19,59 @@
 #' taxize::classification(tsn_acer, db = "itis")
 #' @export
 
-search_taxa <- function( query = NULL, original = FALSE, verbose = TRUE, ... ) {
+search_taxa <- function( query = NULL, tsn = NULL, gbif = NULL, eol = NULL, col =  NULL, bold = NULL, ncbi = NULL, original = FALSE, verbose = TRUE, ... ) {
+    
+    # prep query
+    request <- list(
+      q = query,
+      tsn = tsn,
+      gbif = gbif, 
+      eol = eol, 
+      col =  col, 
+      bold = bold, 
+      ncbi = ncbi
+    )
 
-    stopifnot(!is.null(query) & is.character(query))
+    if (sum(!sapply(request,is.null)) > 1) {
+      stop("Query multiple criteria not allowed")
+    } else if (sum(!sapply(request,is.null)) == 0) {
+      stop("Query unspecified")
+    }
 
-    if (!original) {
+    if(is.character(query)){
+      if(verbose) message("Full text search")
+    } 
 
-        taxa <- as.data.frame(get_gen(endpoints()$taxonomy, query = list(q = query)))
+    if (!is.null(query) & original) {
 
-        if (length(taxa)) {
-          tmp_nodes <- do.call(rbind,
-              purrr::map(sapply(taxa$id,
-                function(x) get_from_fkey(endpoints()$node, taxonomy_id = x )), "body"))
-
-          # Add original publication name for the taxa
-          taxa$original_name <- tmp_nodes$original_name
-          # Retrieve network in which taxa are involved
-          network_ids <- tmp_nodes$network_id
-        } else network_ids <- NULL
+      taxa <- as.data.frame(get_gen(endpoints()$node, query = request))
+      network_ids <- taxa$network_id
 
     } else {
 
-        taxa <- as.data.frame(get_gen(endpoints()$node,
-          query = list(q = query)))
-        network_ids <- taxa$network_id
+      taxa <- as.data.frame(get_gen(endpoints()$taxonomy, query = request))
+
+      if (length(taxa)) {
+        tmp_nodes <- do.call(rbind,
+            purrr::map(sapply(taxa$id,
+              function(x) get_from_fkey(endpoints()$node, taxonomy_id = x )), "body"))
+
+        # Add original publication name for the taxa
+        taxa$original_name <- tmp_nodes$original_name
+        # Retrieve network in which taxa are involved
+        network_ids <- tmp_nodes$network_id
+      } else network_ids <- NULL
 
     }
 
     if (length(network_ids)) {
-        taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
+      taxa$networks <- as.data.frame(get_singletons(endpoints()$network, network_ids))
     } else {
       taxa <- data.frame(NULL)
     }
 
 
-    if (verbose) message(sprintf("Found %s taxa", nrow(taxa)))
+    if (verbose) message(sprintf("Found %s taxa involved in %s network(s)", nrow(taxa), length(network_ids)))
 
     class(taxa) <- append(class(taxa), "mgSearchTaxa")
     taxa
